5da58e5eddd73b0bae98153d5abdfa221ee99f59
Deviant <michaelngo1337@yahoo.com>
2021-05-24 22:13:31 -0700

    display 5 day forecast information

--- Assets/script.js
+++ Assets/script.js
@@ -9,12 +9,17 @@ document.getElementById('searchButton').addEventListener('click', () => {
       axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${response.data.name}&appid=94f8ea24d2c4cd33d640135d8ee0a8d9&units=imperial`)
       .then(response3 => {
         let array = response3.data.list //array of 40 forecasts, each with a 3 hour interval
-        let myMaxTemp, maxTemps = []
+        let myMaxTemp, maxTemps = [], forecasts = []
         myMaxTemp = array[0].main.temp_max
         for (let i = 1; i <= array.length; i++) {
           if (i % 8 === 0) {
-            maxTemps.push(myMaxTemp)
-            console.log(`Index: ${i}`)
+            forecasts.push({
+              date: array[i - 1].dt_txt.split(' ')[0],
+              weather: array[i - 1].weather[0].main,
+              temp: myMaxTemp,
+              humidity: array[i-1].main.humidity
+            })
+            // maxTemps.push(myMaxTemp)
             myMaxTemp = 0
           }
           if (array[i-1].main.temp_max > myMaxTemp) {
@@ -22,17 +27,25 @@ document.getElementById('searchButton').addEventListener('click', () => {
           }
         }
 
-          document.getElementById('weatherData').innerHTML = `
+        document.getElementById('weatherData').innerHTML = `
           <h1>${response.data.name}</h1>
           <p>Temperature: ${response.data.main.temp} ℉</p>
           <p>Humidity: ${response.data.main.humidity}%</p>
           <p>Wind Speed: ${response.data.wind.speed} MPH</p>
           <p>UV Index: ${response2.data.current.uvi}</p>
         `
+        forecasts.forEach(forecast => {
+          document.getElementById('forecasts').innerHTML += `
+            <div class="col">
+              <h1>${forecast.date}</h1>
+              <p>${forecast.weather}</p>
+              <p>${forecast.temp}</p>
+              <p>${forecast.humidity}</p>
+            </div>
+          `
+        })
 
 
-
-        
         // for debugging
         maxTemps.forEach(temp => {
           console.log(`${temp}\n`)
