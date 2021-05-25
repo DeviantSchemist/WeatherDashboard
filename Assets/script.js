578bf293d1e518800ac08fcc5a285d80a71211d9
Deviant <michaelngo1337@yahoo.com>
2021-05-24 17:38:52 -0700

    displayed city weather data including UV index

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,21 @@
+document.getElementById('weatherData').innerHTML = 'Weather Data Displays Here'
+
 document.getElementById('searchButton').addEventListener('click', () => {
   document.getElementById('searchInput').innerHTML = ''
-  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('searchInput').value}&appid=94f8ea24d2c4cd33d640135d8ee0a8d9`)
+  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('searchInput').value}&appid=94f8ea24d2c4cd33d640135d8ee0a8d9&units=imperial`)
   .then(response => {
-    console.log(response.data)
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&exclude=minutely,hourly,daily,alerts&appid=94f8ea24d2c4cd33d640135d8ee0a8d9&units=imperial`)
+    .then(response2 => {
+      document.getElementById('weatherData').innerHTML = `
+        <h1>${response.data.name}</h1>
+        <p>Temperature: ${response.data.main.temp} ℉</p>
+        <p>Humidity: ${response.data.main.humidity}%</p>
+        <p>Wind Speed: ${response.data.wind.speed} MPH</p>
+        <p>UV Index: ${response2.data.current.uvi}</p>
+      `
+      // console.log(response2.data.current)
+    })
+    .catch(err => console.error(err))
   })
   .catch(err => console.error(err))
 })
