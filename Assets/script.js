fd9997d15ffa99738fcd9a5ee5a408ed136e3ff1
Deviant <michaelngo1337@yahoo.com>
2021-05-24 21:43:13 -0700

    added max temperatures from 5 day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -9,7 +9,18 @@ document.getElementById('searchButton').addEventListener('click', () => {
       axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${response.data.name}&appid=94f8ea24d2c4cd33d640135d8ee0a8d9&units=imperial`)
       .then(response3 => {
         let array = response3.data.list //array of 40 forecasts, each with a 3 hour interval
-        
+        let myMaxTemp, maxTemps = []
+        myMaxTemp = array[0].main.temp_max
+        for (let i = 1; i <= array.length; i++) {
+          if (i % 8 === 0) {
+            maxTemps.push(myMaxTemp)
+            console.log(`Index: ${i}`)
+            myMaxTemp = 0
+          }
+          if (array[i-1].main.temp_max > myMaxTemp) {
+            myMaxTemp = array[i-1].main.temp_max
+          }
+        }
 
           document.getElementById('weatherData').innerHTML = `
           <h1>${response.data.name}</h1>
@@ -18,6 +29,14 @@ document.getElementById('searchButton').addEventListener('click', () => {
           <p>Wind Speed: ${response.data.wind.speed} MPH</p>
           <p>UV Index: ${response2.data.current.uvi}</p>
         `
+
+
+
+        
+        // for debugging
+        maxTemps.forEach(temp => {
+          console.log(`${temp}\n`)
+        })
         console.log(array)
       })
       .catch(err => console.error(err))
